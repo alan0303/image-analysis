d28ff8af770f151df4775d49e640627c9cf83ee8
alan0303 <52855632+alan0303@users.noreply.github.com>
2023-12-25 01:09:51 +0800

    Add files via upload

---
+++ config/microsoft_cognitive_services.py
@@ -0,0 +1,4 @@
+# define our Microsoft Cognitive Services subscription key
+SUBSCRIPTION_KEY = "e59048248c5540faa9f9d5c2355a68aa"
+# define the ACS endpoint
+ENDPOINT_URL = "https://gaiacomputervision.cognitiveservices.azure.com/"
